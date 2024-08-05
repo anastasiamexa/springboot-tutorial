89ee42f6f296d8ac8d14a5206d48717686c2e739
Anastasia Mexa <anastasia.mexa@yahoo.gr>
2024-08-05 13:50:31 +0300

    component scanning

---
+++ 02-spring-boot-spring-core/02-component-scanning/src/main/java/com/springboot/demo/rest/DemoController.java
@@ -0,0 +1,26 @@
+package com.springboot.demo.rest;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.springboot.demo.common.Coach;
+
+@RestController
+public class DemoController {
+
+    // Define a private field for the dependency
+    private Coach coach;
+
+    // Define a constructor for dependency injection
+    @Autowired
+    public DemoController(Coach theCoach) {
+        coach = theCoach;
+    }
+
+    // Define a method to expose a REST endpoint
+    @GetMapping("/dailyWorkout")
+    public String getDailyWorkout() {
+        return coach.getDailyWorkout();
+    }
+}
