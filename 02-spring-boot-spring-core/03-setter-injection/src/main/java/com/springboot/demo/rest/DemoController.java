2910ba3e3cf0f43af74becb5de423e00f6dccf39
Anastasia Mexa <anastasia.mexa@yahoo.gr>
2024-08-06 13:59:16 +0300

    setter injection

---
+++ 02-spring-boot-spring-core/03-setter-injection/src/main/java/com/springboot/demo/rest/DemoController.java
@@ -0,0 +1,26 @@
+package com.springboot.demo.rest;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.springboot.demo.common.Coach;
+
+@RestController
+public class DemoController {
+
+    // Define a private field for the dependency
+    private Coach coach;
+
+    // Define a setter injection
+    @Autowired
+    public void setCoach(Coach coach) {
+        this.coach = coach;
+    }
+
+    // Define a method to expose a REST endpoint
+    @GetMapping("/dailyWorkout")
+    public String getDailyWorkout() {
+        return coach.getDailyWorkout();
+    }
+}
