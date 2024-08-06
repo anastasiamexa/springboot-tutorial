d6170f35fb5da732ed41cbe91034a1859372845e
Anastasia Mexa <anastasia.mexa@yahoo.gr>
2024-08-06 14:13:05 +0300

    using primary annotation

---
+++ 02-spring-boot-spring-core/05-primary/src/main/java/com/springboot/demo/common/CricketCoach.java
@@ -0,0 +1,11 @@
+package com.springboot.demo.common;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class CricketCoach implements Coach {
+    @Override
+    public String getDailyWorkout() {
+        return "Practice fast bowling for 15 minutes";
+    }
+}
---
+++ 02-spring-boot-spring-core/05-primary/src/main/java/com/springboot/demo/common/TennisCoach.java
@@ -0,0 +1,12 @@
+package com.springboot.demo.common;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class TennisCoach implements Coach {
+
+    @Override
+    public String getDailyWorkout() {
+        return "Practice your backhand volley";
+    }
+}
