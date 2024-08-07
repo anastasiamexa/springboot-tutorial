5f949c6b5b48c75f3e2d91dbdb0afcca829a066f
Anastasia Mexa <anastasia.mexa@yahoo.gr>
2024-08-07 16:08:27 +0300

    * Add SQL scripts for creating user and student tables * Created class Student with JPA annotations * Added to application.properties code to minimize server logs.

---
+++ 03-spring-boot-hibernate-jpa-crud/01-crud-demo-student/src/main/java/com/springboot/demo/DemoApplication.java
@@ -0,0 +1,21 @@
+package com.springboot.demo;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+	@Bean
+	public CommandLineRunner commandLineRunner() {
+		return args -> {
+			System.out.println("Hello World");
+		};
+	}
+}
