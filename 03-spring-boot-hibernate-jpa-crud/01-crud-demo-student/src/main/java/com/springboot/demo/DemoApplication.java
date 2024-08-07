70f32d99fc61da3bc4c9bdca036de8df4503ffe0
Anastasia Mexa <anastasia.mexa@yahoo.gr>
2024-08-07 16:34:44 +0300

    Add method to create multiple students.

--- 03-spring-boot-hibernate-jpa-crud/01-crud-demo-student/src/main/java/com/springboot/demo/DemoApplication.java
+++ 03-spring-boot-hibernate-jpa-crud/01-crud-demo-student/src/main/java/com/springboot/demo/DemoApplication.java
@@ -18,14 +18,40 @@ public class DemoApplication {
 	@Bean
 	public CommandLineRunner commandLineRunner(StudentDAO studentDAO) {
 		return args -> {
-			// create a student
-			Student student = new Student("John", "Doe", "john@doe.com");
-
-			// save the student
-			studentDAO.save(student);
-
-			// display the id of the saved student
-			System.out.println("Saved student id: " + student.getId());
+			// createStudent(studentDAO);
+			createMultipleStudents(studentDAO);
 		};
+	}
+
+	private void createMultipleStudents(StudentDAO studentDAO) {
+		// create multiple students
+		System.out.println("Creating multiple students...");
+		Student student1 = new Student("Anastasia", "Mexa", "anastasia@mexa.com");
+		Student student2 = new Student("Michael", "Jackson", "michael@jackson.com");
+		Student student3 = new Student("Janet", "Jackson", "janet@jackson.com");
+
+		// save the students
+		System.out.println("Saving the students...");
+		studentDAO.save(student1);
+		studentDAO.save(student2);
+		studentDAO.save(student3);
+
+		// display the ids of the saved students
+		System.out.println("Saved student1 id: " + student1.getId());
+		System.out.println("Saved student2 id: " + student2.getId());
+		System.out.println("Saved student3 id: " + student3.getId());
+	}
+
+	private void createStudent(StudentDAO studentDAO) {
+		// create a student
+		System.out.println("Creating a new student...");
+		Student student = new Student("John", "Doe", "john@doe.com");
+
+		// save the student
+		System.out.println("Saving the student...");
+		studentDAO.save(student);
+
+		// display the id of the saved student
+		System.out.println("Saved student id: " + student.getId());
 	}
 }
