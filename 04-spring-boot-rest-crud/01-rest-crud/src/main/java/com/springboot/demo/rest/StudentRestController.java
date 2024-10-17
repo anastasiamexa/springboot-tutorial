6965140a70f9f6e13cef3d6d8911f221c08bdc58
Anastasia Mexa <anastasia.mexa@yahoo.gr>
2024-10-17 15:06:51 +0300

    Refactor StudentRestController to separate exception handling logic into a dedicated class

--- 04-spring-boot-rest-crud/01-rest-crud/src/main/java/com/springboot/demo/rest/StudentRestController.java
+++ 04-spring-boot-rest-crud/01-rest-crud/src/main/java/com/springboot/demo/rest/StudentRestController.java
@@ -46,28 +46,4 @@ public class StudentRestController {
 
         return students.get(studentId);
     }
-
-    // Add an exception handler using @ExceptionHandler
-    @ExceptionHandler
-    public ResponseEntity<StudentErrorResponse> handleException(StudentNotFoundException exc) {
-        // create a StudentErrorResponse
-        StudentErrorResponse error = new StudentErrorResponse();
-        error.setStatus(HttpStatus.NOT_FOUND.value());
-        error.setMessage(exc.getMessage());
-        error.setTimeStamp(System.currentTimeMillis());
-
-        return new ResponseEntity<>(error, HttpStatus.NOT_FOUND);
-    }
-
-    // Add another exception handler to catch any exception (catch all)
-    @ExceptionHandler
-    public ResponseEntity<StudentErrorResponse> handleException(Exception exc) {
-        // create a StudentErrorResponse
-        StudentErrorResponse error = new StudentErrorResponse();
-        error.setStatus(HttpStatus.BAD_REQUEST.value());
-        error.setMessage(exc.getMessage());
-        error.setTimeStamp(System.currentTimeMillis());
-
-        return new ResponseEntity<>(error, HttpStatus.BAD_REQUEST);
-    }
 }
