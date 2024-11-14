e7d94ad399052396033cae36454a8deb46ebd026
Anastasia Mexa <anastasia.mexa@yahoo.gr>
2024-11-14 14:02:05 +0200

    Implement JDBC authentication and update security filter chain for role-based access control

--- 04-spring-boot-rest-crud/05-rest-security/src/main/java/com/luv2code/springboot/cruddemo/security/DemoSecurityConfig.java
+++ 04-spring-boot-rest-crud/05-rest-security/src/main/java/com/luv2code/springboot/cruddemo/security/DemoSecurityConfig.java
@@ -1,19 +1,45 @@
 package com.luv2code.springboot.cruddemo.security;
 
+import javax.sql.DataSource;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
+import org.springframework.security.provisioning.UserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class DemoSecurityConfig {
+    // Add support for JDBC authentication
+    @Bean
+    public UserDetailsManager userDetailsManager(DataSource dataSource) {
+        return new JdbcUserDetailsManager(dataSource);
+    }
+    
 
     @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests(configurer -> configurer
+            .requestMatchers(HttpMethod.GET, "/api/employees").hasRole("EMPLOYEE")
+            .requestMatchers(HttpMethod.GET, "/api/employees/**").hasRole("EMPLOYEE")
+            .requestMatchers(HttpMethod.POST, "/api/employees").hasRole("MANAGER")
+            .requestMatchers(HttpMethod.PUT, "/api/employees/**").hasRole("MANAGER")
+            .requestMatchers(HttpMethod.DELETE, "/api/employees/**").hasRole("ADMIN")
+        );
+
+        // Use httpBasic authentication
+        http.httpBasic(Customizer.withDefaults());
+
+        // Disable CSRF
+        http.csrf(csrf -> csrf.disable());
+
+        return http.build();
+    }
+
+    /*@Bean
     public InMemoryUserDetailsManager userDetailsManager() {
         UserDetails michael = User.builder()
             .username("michael")
@@ -34,24 +60,5 @@ public class DemoSecurityConfig {
             .build();
 
         return new InMemoryUserDetailsManager(michael, janet, stacy);
-    }
-
-    @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http.authorizeHttpRequests(configurer -> configurer
-            .requestMatchers(HttpMethod.GET, "/api/employees").hasRole("EMPLOYEE")
-            .requestMatchers(HttpMethod.GET, "/api/employees/**").hasRole("EMPLOYEE")
-            .requestMatchers(HttpMethod.POST, "/api/employees").hasRole("MANAGER")
-            .requestMatchers(HttpMethod.PUT, "/api/employees/**").hasRole("MANAGER")
-            .requestMatchers(HttpMethod.DELETE, "/api/employees/**").hasRole("ADMIN")
-        );
-
-        // Use httpBasic authentication
-        http.httpBasic(Customizer.withDefaults());
-
-        // Disable CSRF
-        http.csrf(csrf -> csrf.disable());
-
-        return http.build();
-    }
+    }*/
 }
