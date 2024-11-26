26675b469a56632fbde5cf58b888a622003bc7ef
Anastasia Mexa <anastasia.mexa@yahoo.gr>
2024-11-26 23:07:48 +0200

    Read form data and displaying them

--- 05-spring-boot-spring-mvc/01-thymeleaf-demo/src/main/java/com/springboot/thymeleafdemo/controller/HelloWorldController.java
+++ 05-spring-boot-spring-mvc/01-thymeleaf-demo/src/main/java/com/springboot/thymeleafdemo/controller/HelloWorldController.java
@@ -1,8 +1,11 @@
 package com.springboot.thymeleafdemo.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import jakarta.servlet.http.HttpServletRequest;
 
 @Controller
 public class HelloWorldController {
@@ -15,6 +18,21 @@ public class HelloWorldController {
   @GetMapping("/processForm")
   // method to process the HTML form
   public String processForm() {
+    return "helloworld";
+  }
+
+  // method to read form data and add data to the model
+  @GetMapping("/processForm2")
+  public String letsShoutDude(HttpServletRequest request, Model model) {
+    // read the request parameter from the HTML form
+    String name = request.getParameter("studentName");
+    // convert the data to all caps
+    name = name.toUpperCase();
+    // create the message
+    String result = "Yo! " + name;
+    // add message to the model
+    model.addAttribute("message", result);
+    
     return "helloworld";
   }
 }
