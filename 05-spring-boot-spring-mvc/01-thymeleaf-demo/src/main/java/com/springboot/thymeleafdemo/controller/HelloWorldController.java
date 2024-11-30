999aa00013d64ef7cf08fd655bdeaaeaf63fc8ed
Anastasia Mexa <anastasia.mexa@yahoo.gr>
2024-11-30 15:48:18 +0200

    Refactor HelloWorldController to use @GetMapping and @PostMapping for endpoint definitions; update form method to POST in helloworld-form.html

--- 05-spring-boot-spring-mvc/01-thymeleaf-demo/src/main/java/com/springboot/thymeleafdemo/controller/HelloWorldController.java
+++ 05-spring-boot-spring-mvc/01-thymeleaf-demo/src/main/java/com/springboot/thymeleafdemo/controller/HelloWorldController.java
@@ -2,6 +2,8 @@ package com.springboot.thymeleafdemo.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -10,7 +12,7 @@ import jakarta.servlet.http.HttpServletRequest;
 @Controller
 public class HelloWorldController {
   // method to show the initial HTML form
-  @RequestMapping("/showForm")
+  @GetMapping("/showForm")
   public String showForm() {
     return "helloworld-form";
   }
@@ -37,7 +39,7 @@ public class HelloWorldController {
   }
 
   // method to read form data and add data to the model via @RequestParam
-  @RequestMapping("/processForm3")
+  @PostMapping("/processForm3")
   public String processForm3(@RequestParam("studentName") String name, Model model) {
     // convert the data to all caps
     name = name.toUpperCase();
