8387826bdddf690190dd0a0324f35b2f8af5b49e
Anastasia Mexa <anastasia.mexa@yahoo.gr>
2024-12-03 16:02:55 +0200

    Add JDBC support for user details management and configure MySQL datasource

--- 08-spring-boot-spring-mvc-security/01-spring-boot-spring-mvc-security/src/main/java/com/springboot/demosecurity/security/DemoSecurityConfig.java
+++ 08-spring-boot-spring-mvc-security/01-spring-boot-spring-mvc-security/src/main/java/com/springboot/demosecurity/security/DemoSecurityConfig.java
@@ -1,17 +1,44 @@
 package com.springboot.demosecurity.security;
 
+import javax.sql.DataSource;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
+import org.springframework.security.provisioning.UserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class DemoSecurityConfig {
 
+    // add support for JDBC
     @Bean
+    public UserDetailsManager userDetailsManager(DataSource dataSource) {
+
+        return new JdbcUserDetailsManager(dataSource);
+    }
+
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests(configurer -> configurer
+                .requestMatchers("/").hasRole("EMPLOYEE")
+                .requestMatchers("/leaders/**").hasRole("MANAGER")
+                .requestMatchers("/systems/**").hasRole("ADMIN")
+                .anyRequest().authenticated())
+                .formLogin(form -> form
+                        .loginPage("/showMyLoginPage")
+                        .loginProcessingUrl("/authenticateTheUser")
+                        .permitAll() // Allow everyone to see login page. No need to be logged in.
+                )
+                .logout(logout -> logout.permitAll()) // Redirect to login page after logout by default.
+                .exceptionHandling(configurer -> configurer.accessDeniedPage("/access-denied"));
+
+        return http.build();
+    }
+
+    /*@Bean
     public InMemoryUserDetailsManager userDetailsManager() {
         UserDetails michael = User.builder()
                 .username("michael")
@@ -32,23 +59,5 @@ public class DemoSecurityConfig {
                 .build();
 
         return new InMemoryUserDetailsManager(michael, janet, stacy);
-    }
-
-    @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http.authorizeHttpRequests(configurer -> configurer
-                .requestMatchers("/").hasRole("EMPLOYEE")
-                .requestMatchers("/leaders/**").hasRole("MANAGER")
-                .requestMatchers("/systems/**").hasRole("ADMIN")
-                .anyRequest().authenticated())
-                .formLogin(form -> form
-                        .loginPage("/showMyLoginPage")
-                        .loginProcessingUrl("/authenticateTheUser")
-                        .permitAll() // Allow everyone to see login page. No need to be logged in.
-                )
-                .logout(logout -> logout.permitAll()) // Redirect to login page after logout by default.
-                .exceptionHandling(configurer -> configurer.accessDeniedPage("/access-denied"));
-
-        return http.build();
-    }
+    }*/
 }
