cf4bcbacd7f3f85c2038736f0cf9472c3c4730b0
Anastasia Mexa <anastasia.mexa@yahoo.gr>
2024-12-03 14:51:11 +0200

    Add custom login page and configure security filter chain

--- 08-spring-boot-spring-mvc-security/01-spring-boot-spring-mvc-security/src/main/java/com/springboot/demosecurity/security/DemoSecurityConfig.java
+++ 08-spring-boot-spring-mvc-security/01-spring-boot-spring-mvc-security/src/main/java/com/springboot/demosecurity/security/DemoSecurityConfig.java
@@ -2,9 +2,11 @@ package com.springboot.demosecurity.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class DemoSecurityConfig {
@@ -30,5 +32,18 @@ public class DemoSecurityConfig {
                 .build();
 
         return new InMemoryUserDetailsManager(michael, janet, stacy);
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests(configurer -> configurer
+                .anyRequest().authenticated())
+                .formLogin(form -> form
+                        .loginPage("/showMyLoginPage")
+                        .loginProcessingUrl("/authenticateTheUser")
+                        .permitAll() // Allow everyone to see login page. No need to be logged in.
+                );
+
+        return http.build();
     }
 }
